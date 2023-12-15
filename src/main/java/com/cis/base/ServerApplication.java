5fe655ee0afe4b9720157cfae2c2bac1b4445668
DEV4Sep <mr.yisivlay@gmail.com>
2023-12-15 16:39:05 +0700

    added file logback-spring.

--- src/main/java/com/cis/base/ServerApplication.java
+++ src/main/java/com/cis/base/ServerApplication.java
@@ -2,12 +2,15 @@ package com.cis.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication application = new SpringApplication(ServerApplication.class);
+		application.setApplicationStartup(new BufferingApplicationStartup(2048));
+		application.run(args);
 	}
 
 }
